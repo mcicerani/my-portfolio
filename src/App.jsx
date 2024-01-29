9ac0403d67be1a66cddeeaaae9eb0181eff72890
Matteo Cicerani <103743024+mcicerani@users.noreply.github.com>
2024-01-29 20:57:44 +0000

    Image update

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const siteProps = {
   youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#000000";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,13 +10,13 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a computer science student studying at Sapienza University. I enjoy creating unique and simplistic websites in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -34,7 +34,6 @@ const skillsList = [
   "Web design",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
 ];
